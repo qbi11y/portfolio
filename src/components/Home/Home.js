ec5dbf398bb7d2ebf31a1bef8d3a735a0244a4cf
James Thompson <qbi11y@gmail.com>
2022-03-01 22:30:11 -0600

    add project names

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import projects from 'data/projects.json'   
+import projects from 'data/projects.json';   
 import { Link } from "react-router-dom";
 
 const Home = () => (
@@ -14,7 +14,7 @@ const Home = () => (
                         Object.keys(projects).map((project, index) => {
                             console.log(project)
                             return(
-                                <li key={index}><Link to={"/portfolio/project/"+ (index+1)}>{ project }</Link></li>
+                                <li key={index}><Link to={"/portfolio/project/"+ project}>{ projects[index + 1]["name"] }</Link></li>
                             )
                         })
                     }
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,13 +1,14 @@
 import React from "react"
 import { useParams } from "react-router-dom"
 import { Link } from "react-router-dom";
+import projects from 'data/projects.json'; 
 
 const Project = () => {
     let { id } = useParams()
     return(
         <div>
-            <h1>Project here { id }</h1>
-            <p>Project { id } description here</p>
+            <h1>Project here { projects[id]["name"] }</h1>
+            <p>Project { projects[id]["name"] } description here</p>
             <p><Link to="/portfolio">Back</Link></p>
         </div>
     )
