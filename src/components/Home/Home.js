de95d49fe02a55acb0fb4ec0dd98593c6eff500b
James Thompson <qbi11y@gmail.com>
2022-03-02 11:14:14 -0600

    updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,18 +5,18 @@ import { Link } from "react-router-dom";
 const Home = () => (
         <div className="container">
             <section>
+                <h5>whoami: jamesqthompson</h5>
                 <h1>Design Technologist</h1>
                 <p>Statement about design and tech</p>
             </section>
             <section>                
                 <ul>
                     {
-                        Object.keys(projects).map((project, index) => {
-                            console.log(project)
-                            return(
+                        Object.keys(projects).map((project, index) => 
+                            (
                                 <li key={index}><Link to={"/portfolio/project/"+ project}>{ projects[index + 1]["name"] }</Link></li>
                             )
-                        })
+                        )
                     }
                 </ul>
                 
