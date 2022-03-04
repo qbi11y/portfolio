959e6da57413b4178ab499b9037a538dac719863
James Thompson <qbi11y@gmail.com>
2022-03-04 17:35:55 -0600

    removed Link

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import projects from 'data/projects.json';
 import content from 'data/content.json';  
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
     const navigate = useNavigate()
